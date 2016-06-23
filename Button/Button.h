8db9ab8c3f46d82ffbd62138bc574621fd96b213
Or amit <oramit88@gmail.com>
2016-06-23 12:14:28 +0300

    changing the code from liza

--- Button/Button.h
+++ Button/Button.h
@@ -11,10 +11,7 @@ public:
 	Button(int width):Label(width,""){}
 	void draw(Graphics graphics, int left, int top, int layer);
 	void SetValue(string value) { Label::SetText(value); }
-<<<<<<< HEAD
 	bool canGetFocus() { return false; }
-=======
->>>>>>> d9b1bcf2e745e7573b4ba23f428e48e86b21f68c
 	void AddListener(MouseListener &listener) { listeners.push_back(&listener); }
 	void mousePressed(int x, int y, bool ifFirstButton);
 	~Button();
--- TextBox/TextBox.h
+++ TextBox/TextBox.h
@@ -8,17 +8,12 @@ class TextBox :
 	string _value = "";
 	int textSize;
 	int maxSize;
-<<<<<<< HEAD
 	int logicalPosition = 0;
 	Graphics _graphics;
-=======
-//	COORD position;
->>>>>>> d9b1bcf2e745e7573b4ba23f428e48e86b21f68c
 public:
 	virtual void draw(Graphics graphics, int left, int top, int layer);
 	virtual void keyDown(WORD code, CHAR chr);
 	virtual void mousePressed(int x, int y, bool ifFirstButton) {};
-<<<<<<< HEAD
 	virtual bool canGetFocus() { return true; }
 
 	string GetValue() { return _value; }
@@ -28,31 +23,5 @@ public:
 //	void nowInFocus() {
 //		_graphics.setCursorVisibility(true);
 //	}
-=======
-	virtual bool canGetFocus() {	return true;}
-
-//	virtual void keyDown(KEY_EVENT_RECORD keyEvent);
-//	virtual void mouseEvent(MOUSE_EVENT_RECORD mouseEvent);
-	string GetValue() { return _value; }
-	//virtual void createFrame() = 0; // replace to draw
-//	virtual void draw();
-//	virtual void hide();
-	TextBox(int width):Control(width,3){}
-	void SetText(string value) { _value = value; }
-	string GetText() { return _value; }
-	/*
-	TextBox(COORD coord, DWORD backgroundColor, DWORD textColor, HANDLE handle, int width, string text = "")
-		:Component(coord, backgroundColor, textColor, handle) {
-		_text = text;
-		_position.setHeight(3);
-		_position.setWidth(width);
-		textSize = text.size();
-		maxSize = width - 2;
-		position = _position.getStartCord();
-		position.X++;
-		position.Y++;
-	}
-*/
->>>>>>> d9b1bcf2e745e7573b4ba23f428e48e86b21f68c
 	~TextBox();
 };
